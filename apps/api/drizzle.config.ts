1128cf8f3d1675cc1582eb5b1b3b7ff8dfc28a5e
nka21 <133028205+nka21@users.noreply.github.com>
2025-07-24 03:46:17 +0900

    fix: 環境変数がなかった場合のハンドリングを実装

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -2,14 +2,23 @@ import { defineConfig } from 'drizzle-kit';
 
 import 'dotenv/config';
 
+function getEnvVariable(name: string): string {
+  const value = process.env[name];
+  if (!value) {
+    throw new Error(`Environment variable ${name} is required but not set.`);
+  }
+
+  return value;
+}
+
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts',
   dialect: 'sqlite',
   driver: 'd1-http',
   dbCredentials: {
-    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-    databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-    token: process.env.CLOUDFLARE_API_TOKEN!,
+    accountId: getEnvVariable('CLOUDFLARE_ACCOUNT_ID'),
+    databaseId: getEnvVariable('CLOUDFLARE_DATABASE_ID'),
+    token: getEnvVariable('CLOUDFLARE_API_TOKEN'),
   },
 });

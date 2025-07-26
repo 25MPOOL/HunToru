6df0cc8b8ff728fb0830ba3a76e1e93743dd2a6d
nka21 <133028205+nka21@users.noreply.github.com>
2025-07-26 23:13:06 +0900

    feat: 吹き出しコンポーネントを作成

---
+++ apps/web/src/components/ui/PixelBubble.tsx
@@ -0,0 +1,112 @@
+import type { SVGProps } from 'react';
+
+type PixelBubbleProps = SVGProps<SVGSVGElement>;
+
+export const PixelBubble = (props: PixelBubbleProps) => {
+  return (
+    <>
+      <svg
+        viewBox="0 0 516 191"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+      >
+        <path
+          d="M435.69 10.2401H80.42L50.34 20.2801L30.27 30.3001L20.25 40.3401L10.22 60.3901V90.5001L20.25 110.56L30.27 120.59L50.34 130.63L60.37 140.65L415.64 150.69L425.66 160.73L435.69 170.75L445.72 180.78H455.74V130.63H465.78L485.84 120.59L495.86 110.56L505.89 90.5001V60.3901L495.86 40.3401L485.84 30.3001L465.78 20.2801L435.69 10.2401Z"
+          fill="currentColor"
+        />
+        <path d="M10.22 80.46H20.25V90.5H10.22V80.46Z" fill="#DADADA" />
+        <path d="M20.25 90.5H30.29V100.54H20.25V90.5Z" fill="#DADADA" />
+        <path d="M20.25 100.52H30.29V110.56H20.25V100.52Z" fill="#DADADA" />
+        <path d="M30.27 100.52H40.3V110.56H30.27V100.52Z" fill="#DADADA" />
+        <path d="M30.27 110.56H40.3V120.59H30.27V110.56Z" fill="#DADADA" />
+        <path d="M40.3 110.56H50.34V120.59H40.3V110.56Z" fill="#DADADA" />
+        <path d="M50.34 110.56H60.37V120.59H50.34V110.56Z" fill="#DADADA" />
+        <path d="M50.34 120.59H60.37V130.63H50.34V120.59Z" fill="#DADADA" />
+        <path d="M60.37 120.59H70.41V130.63H60.37V120.59Z" fill="#DADADA" />
+        <path d="M60.37 130.63H70.41V140.67H60.37V130.63Z" fill="#DADADA" />
+        <path d="M70.39 130.63H80.42V140.67H70.39V130.63Z" fill="#DADADA" />
+        <path d="M80.42 130.63H405.61V140.67H80.42V130.63Z" fill="#DADADA" />
+        <path d="M405.61 130.63H415.64V140.67H405.61V130.63Z" fill="#DADADA" />
+        <path d="M415.64 140.65H425.67V150.69H415.64V140.65Z" fill="#DADADA" />
+        <path d="M425.66 150.69H435.69V160.73H425.66V150.69Z" fill="#DADADA" />
+        <path d="M435.69 160.73H445.72V170.76H435.69V160.73Z" fill="#DADADA" />
+        <path d="M445.72 170.75H455.76V180.78H445.72V170.75Z" fill="#DADADA" />
+        <path d="M455.74 120.59H465.78V130.63H455.74V120.59Z" fill="#DADADA" />
+        <path d="M465.78 110.56H475.81V120.59H465.78V110.56Z" fill="#DADADA" />
+        <path d="M475.81 110.56H485.84V120.59H475.81V110.56Z" fill="#DADADA" />
+        <path d="M485.84 100.52H495.88V110.56H485.84V100.52Z" fill="#DADADA" />
+        <mask
+          id="mask0_20_3"
+          style={{ maskType: 'luminance' }}
+          maskUnits="userSpaceOnUse"
+          x="445"
+          y="130"
+          width="21"
+          height="61"
+        >
+          <path d="M445 130H466V190.89H445V130Z" fill="white" />
+        </mask>
+        <g mask="url(#mask0_20_3)">
+          <path
+            d="M455.74 140.65V180.78H445.72V190.82H465.78V130.63H455.74V140.65Z"
+            fill="black"
+          />
+        </g>
+        <path d="M435.69 170.75H445.72V180.78H435.69V170.75Z" fill="black" />
+        <path d="M425.66 160.73H435.69V170.76H425.66V160.73Z" fill="black" />
+        <path d="M415.64 150.69H425.67V160.73H415.64V150.69Z" fill="black" />
+        <path
+          d="M405.61 140.65H60.37V150.69H415.64V140.65H405.61Z"
+          fill="black"
+        />
+        <path d="M465.78 120.59V130.63H485.84V120.59H465.78Z" fill="black" />
+        <path d="M485.84 110.56H495.88V120.59H485.84V110.56Z" fill="black" />
+        <path d="M485.84 30.3H495.88V40.34H485.84V30.3Z" fill="black" />
+        <path d="M475.81 30.3H485.84V20.28H465.78V30.3H475.81Z" fill="black" />
+        <path
+          d="M445.72 20.2801H465.78V10.2401H435.69V20.2801H445.72Z"
+          fill="black"
+        />
+        <path
+          d="M495.86 100.52V110.56H505.89V90.5H495.86V100.52Z"
+          fill="black"
+        />
+        <path d="M495.86 60.39H505.89V40.34H495.86V60.39Z" fill="black" />
+        <path d="M505.89 80.46V90.5H515.93V60.39H505.89V80.46Z" fill="black" />
+        <path d="M20.25 30.3H30.29V40.34H20.25V30.3Z" fill="black" />
+        <path d="M50.34 30.3V20.28H30.27V30.3H50.34Z" fill="black" />
+        <path
+          d="M70.39 20.2801H80.42V10.2401H50.34V20.2801H70.39Z"
+          fill="black"
+        />
+        <path d="M20.25 50.37V40.34H10.22V60.39H20.25V50.37Z" fill="black" />
+        <path d="M20.25 110.56H30.29V120.59H20.25V110.56Z" fill="black" />
+        <path d="M40.3 120.59H30.27V130.63H50.34V120.59H40.3Z" fill="black" />
+        <path d="M50.34 130.63H60.37V140.67H50.34V130.63Z" fill="black" />
+        <path d="M20.25 90.5H10.22V110.56H20.25V90.5Z" fill="black" />
+        <mask
+          id="mask1_20_3"
+          style={{ maskType: 'luminance' }}
+          maskUnits="userSpaceOnUse"
+          x="0"
+          y="60"
+          width="11"
+          height="31"
+        >
+          <path d="M0.23999 60H11V91H0.23999V60Z" fill="white" />
+        </mask>
+        <g mask="url(#mask1_20_3)">
+          <path
+            d="M10.22 80.46V60.39H0.190002V90.5H10.22V80.46Z"
+            fill="black"
+          />
+        </g>
+        <path
+          d="M415.64 10.24H435.69V0.210022H80.42V10.24H415.64Z"
+          fill="black"
+        />
+      </svg>
+    </>
+  );
+};
